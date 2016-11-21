a10f54f88697ac46b32ea6f99c6dab3d70b3f8f1
6m1n6r <6m1n6r@gmail.com>
2016-11-21 22:37:36 +0800

    init project

---
+++ lingling/views/site/contact.php
@@ -0,0 +1,66 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\ContactForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\captcha\Captcha;
+
+$this->title = '联系我们';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-contact">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+
+        <div class="alert alert-success">
+            感谢您的联系，我们会尽快回复您的信息。
+        </div>
+
+        <p>
+            <?php if (Yii::$app->mailer->useFileTransport): ?>
+                Because the application is in development mode, the email is not sent but saved as
+                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
+                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
+                application component to be false to enable email sending.
+            <?php endif; ?>
+        </p>
+
+    <?php else: ?>
+
+        <p>
+            如果您还有任何的疑问，可以在以下表格填写信息联系我们。 
+            谢谢。
+        </p>
+
+        <div class="row">
+            <div class="col-lg-5">
+
+                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+
+                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+
+                    <?= $form->field($model, 'email') ?>
+
+                    <?= $form->field($model, 'subject') ?>
+
+                    <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
+
+                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                    ]) ?>
+
+                    <div class="form-group">
+                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                    </div>
+
+                <?php ActiveForm::end(); ?>
+
+            </div>
+        </div>
+
+    <?php endif; ?>
+</div>
---
+++ lingling/views/site/index.php
@@ -0,0 +1,44 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = '领领——在线失物招领平台';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>失物招领</h1>
+
+        <p class="lead">您有物品丢失了吗？马上发布寻物启事吧！</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">发布寻物启事</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>地点</h2>
+
+                <p>丢失的地点</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">按地点寻找 &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>物品</h2>
+
+                <p>丢失的物品</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">按物品寻找 &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>详情</h2>
+
+                <p>丢失的物品详情</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">自定义 &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
